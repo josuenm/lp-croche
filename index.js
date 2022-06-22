26d5ac34631a0c981cfeff96e62ef226a6c9b70d
josuenm <direct.josue@gmail.com>
2022-06-22 15:19:50 -0300

    Lógica de desaparecer e aparecer do botão fixo foi feita

--- index.js
+++ index.js
@@ -7,3 +7,28 @@ async function api() {
     );
 }
 api();
+
+
+<!-- Fixed Button script -->
+const scrollListener = () => {
+    const element = document.querySelector('.presentation')
+    const position = element.getBoundingClientRect().top.toFixed(0)
+    
+    const fixedButton = document.querySelector('.fixed_button')
+    
+    const button = document.querySelector('.link')
+    const buttonPosition = button.getBoundingClientRect().top.toFixed(0)
+
+
+    if(position < -340 && buttonPosition > 640 || buttonPosition < -30) {
+        if(fixedButton.classList.contains('active')) {
+            return
+        } else {
+            fixedButton.classList.add('active')
+        }
+    } else {
+        fixedButton.classList.remove('active')
+    }
+};
+
+window.addEventListener("scroll", scrollListener);
